2d8cb8cf39b47d0adfca321ebf780a8d1dd485d5
acanel2021 <canelanthony@gmail.com>
2021-06-22 11:51:18 +0200

    Version - Ajout DALException

---
+++ src/fr/eni/dal/DALException.java
@@ -0,0 +1,19 @@
+package fr.eni.dal;
+
+public class DALException extends Exception{
+    public DALException() {
+        super();
+    }
+
+    public DALException(String message) {
+        super(message);
+    }
+
+    public DALException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DALException(Throwable cause) {
+        super(cause);
+    }
+}
