52da950058b15fe7a697b1b23a8ec00965ed02e6
acanel2021 <canelanthony@gmail.com>
2021-06-22 17:27:09 +0200

    Version - Mise à jour Enchere et EnchereDAOJdbcImpl

--- src/fr/eni/bo/Enchere.java
+++ src/fr/eni/bo/Enchere.java
@@ -2,22 +2,45 @@ package fr.eni.bo;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
 import java.util.Date;
 
 @Data
 public class Enchere {
-    private Date dateEnchere;
-    private int montantEnchere;
     private Utilisateur utilisateur;
     private Article article;
 
+    private int noUtilisateur;
+    private int noArticle;
+
+    private Timestamp dateEnchere;
+    private int montantEnchere;
+
+    private String etatEnchere;
+    private int noVendeur;
+
+
     public Enchere() {
+
     }
 
-    public Enchere(Date dateEnchere, int montantEnchere, Utilisateur utilisateur, Article article) {
-        this.dateEnchere = dateEnchere;
-        this.montantEnchere = montantEnchere;
+    public Enchere( int noUtilisateur, int noArticle, Timestamp dateEnchere, int montantEnchere, String etatEnchere, int noVendeur) {
         this.utilisateur = utilisateur;
         this.article = article;
+        this.noUtilisateur = noUtilisateur;
+        this.noArticle = noArticle;
+        this.dateEnchere = dateEnchere;
+        this.montantEnchere = montantEnchere;
+        this.etatEnchere = etatEnchere;
+        this.noVendeur = noVendeur;
+    }
+
+    public Enchere(Utilisateur utilisateur, Article article, Timestamp dateEnchere, int montantEnchere, String etatEnchere, int noVendeur) {
+        this.utilisateur = utilisateur;
+        this.article = article;
+        this.dateEnchere = dateEnchere;
+        this.montantEnchere = montantEnchere;
+        this.etatEnchere = etatEnchere;
+        this.noVendeur = noVendeur;
     }
 }
