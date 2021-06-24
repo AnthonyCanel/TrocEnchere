d07fe66a84e3513ac48713bbcb589e73597122af
acanel2021 <canelanthony@gmail.com>
2021-06-24 12:25:33 +0200

    MaJ DAO et Factory -Ajout Default getutilisateur

--- src/fr/eni/dal/DAOFactory.java
+++ src/fr/eni/dal/DAOFactory.java
@@ -4,17 +4,17 @@ import fr.eni.bo.Utilisateur;
 
 public abstract class DAOFactory {
 
-//    public static DAO getArticleDAO(){
-//        return new ArticleDAOJdbcImpl();
-//    }
-//
-//    public static DAO getCategorieDAO(){
-//        return new CategorieDAOJdbcImpl();
-//    }
-//
-//    public static DAO getEnchereDAO(){
-//        return new EnchereDAOJdbcImpl();
-//    }
+    public static DAO getArticleDAO(){
+        return new ArticleDAOJdbcImpl();
+    }
+
+    public static DAO getCategorieDAO(){
+        return new CategorieDAOJdbcImpl();
+    }
+
+    public static DAO getEnchereDAO(){
+        return new EnchereDAOJdbcImpl();
+    }
 
     public static DAO getRetraitDAO(){
         return new RetraitDAOJdbcImpl();
